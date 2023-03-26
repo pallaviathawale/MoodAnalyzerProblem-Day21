9fd8153f35bb95ca0912af8aae213d6e796720d0
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-03-26 17:03:13 +0530

    Add: MoodAnalyserProblem

---
+++ MoodAnalyserProblem/MoodAnalyse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProblem
+{
+    public class MoodAnalyserClass
+    {
+        public string message { get; set; }
+        public MoodAnalyserClass()
+        {
+            this.message = "";
+        }
+        public MoodAnalyserClass(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyseMood()
+        {
+            if (this.message.Contains("sad", StringComparison.OrdinalIgnoreCase))
+                return "SAD";
+            else
+                return "HAPPY";
+        }
+    }
+
+}
+
